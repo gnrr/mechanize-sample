61596349db347f45ead2316c2360f4e35d3af5f5
gnrr <hideaki.sh@gmail.com>
2016-03-23 02:37:15 +0900

    add links

--- test0.rb
+++ test0.rb
@@ -4,4 +4,10 @@ require 'mechanize'
 
 agent = Mechanize.new
 page = agent.get('http://bestgems.org/')
-p page.body
+# p page.body
+
+# links
+# p page.links              # リンクの一覧
+# p page.links[0].click     # リンク先のページを取得
+link = page.links[0]        # リンクの文字列とリンク先
+p link.text, link.href
